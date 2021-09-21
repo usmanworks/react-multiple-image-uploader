6cd6853235b1e6f70bd5f4754a10e8b85e3fe20c
usmanworks <82507505+usmanworks@users.noreply.github.com>
2021-09-21 16:57:14 +0500

    Change Image limit

--- src/RMIUploader.tsx
+++ src/RMIUploader.tsx
@@ -44,7 +44,7 @@ const RMIUploader: React.FC<RMIUploaderProps> = ({
   const maxNumber = 69;
 
   const onChange = (imageList: ImageListType) => {
-    if (imageList.length > 20) {
+    if (imageList.length > 50) {
       message.warn(warnMessage);
     } else {
       setImages(imageList as never[]);
